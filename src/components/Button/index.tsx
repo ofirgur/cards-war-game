ba5537ce4c230688bfac4bbb8654e366e9e669c5
Ofir Gur <ofirgur@yahoo.com>
2024-03-14 19:06:45 +0200

    update button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,26 @@
-import React, { ReactElement, ReactNode } from 'react'
+import React, { ReactNode, useState } from 'react'
 
 import { ButtonStyled } from './styled';
 
 interface IButton {
     children: ReactNode;
     onClick: () => void;
+    disabled?: boolean;
 }
 
 function Button(props: IButton) {
-    const { children, onClick} = props
+    const { children, onClick, disabled} = props
+    const [isDisabled, setIsDisabled] = useState(disabled);
+    const handleClick = () => {
+        onClick();
+        setIsDisabled(true);
+        setTimeout(() => {
+            setIsDisabled(false);
+        }, 500);
+    };
 
     return (
-        <ButtonStyled onClick={onClick}>{children}</ButtonStyled>
+        <ButtonStyled onClick={handleClick} disabled={isDisabled}>{children}</ButtonStyled>
     )
 }
 
